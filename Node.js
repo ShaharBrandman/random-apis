62969900e15843db3c4ac6fbdc513950f1bed260
ShaharBrandman <67471700+ShaharBrandman@users.noreply.github.com>
2021-06-06 23:05:51 +0300

    Add files via upload

---
+++ Node.js
@@ -0,0 +1,32 @@
+class node {
+    constructor(left, right, value) {
+        this.left = left
+        this.right = right
+        this.value = value
+    }
+
+    getValue() {
+        return this.value
+    }
+
+    getRight() {
+        return this.right
+    }
+
+    getLeft() {
+        return this.left
+    }
+
+    hasValue() {
+        return this.value != undefined
+    }
+
+    hasRight() {
+        return this.right != undefined
+    }
+
+    hasLeft() {
+        return this.left != undefined
+    }
+}
+
